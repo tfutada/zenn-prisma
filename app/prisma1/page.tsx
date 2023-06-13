9aade0f7f00f292d9f679344ea4c2c747d44503e
tfutada <tafutada@gmail.com>
2023-06-13 11:56:46 +0700

    prisma

--- app/prisma1/page.tsx
+++ app/prisma1/page.tsx
@@ -1,5 +1,7 @@
 import {prisma} from '@/lib/prisma';
 
+export const revalidate = 60 // revalidate this segment every 60 seconds
+
 export default async function Posts() {
     const rows = await prisma.user.findMany();
     console.log(rows)
