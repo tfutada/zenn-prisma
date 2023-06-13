1f9b1d5707bc5e42a771377717348b2d06d39c32
tfutada <tafutada@gmail.com>
2023-06-13 19:25:19 +0700

    prisma performance test

--- app/myapi/route.ts
+++ app/myapi/route.ts
@@ -1,7 +1,10 @@
 import {NextResponse} from 'next/server';
 import {prisma} from "@/lib/prisma";
 
-export async function GET() {
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url)
+    const id = searchParams.get('id')
+    console.log(id);
 
     const cart = {
         items: [1, 2, 3]
